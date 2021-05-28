589e16a63d0fcb0fdf5890c38147f13cd6a6b93a
David <destrada2193@gmail.com>
2021-05-28 14:23:26 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+#Jets Application
+
+##
---
+++ src/com/skilldistillery/jets/app/JetsApplication.java
@@ -0,0 +1,20 @@
+package com.skilldistillery.jets.app;
+
+import com.skilldistillery.jets.entity.Airfield;
+
+public class JetsApplication {
+	
+	private Airfield airfield;
+
+	public static void main(String[] args) {
+		
+		JetsApplication jetsapp = new JetsApplication();
+		
+		jetsapp.launch();
+	}
+
+	
+	public void launch() {
+		
+	}
+}
